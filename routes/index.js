fe0031252853651329462b3330f91836638d7649
Juan Falconi <juanfalconi86@gmail.com>
2021-03-01 19:53:54 +0100

    startet the delete

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const ArtistModel = require("../model/artistsModel");
 const LabelModel = require("../model/labelsModel");
 const StylesModel = require("../model/stylesModel");
+const uploader = require("../config/cloudinary");
 
 /* GET home page. */
 router.get("/", async (req, res) => {
@@ -14,7 +15,8 @@ router.get("/dashboard", (req, res) => {
   res.render("dashboard/index");
 });
 
-// Router artists
+// ROUTER ARTISTS
+//Router List Artists
 
 router.get("/dashboard/artist", (req, res, next) => {
   ArtistModel.find()
@@ -26,12 +28,37 @@ router.get("/dashboard/artist", (req, res, next) => {
  
 });
 
+//Router Delete Artists
 router.post("/dashboard/artist/delete/:id", (req, res, next)=>{
   ArtistModel.findByIdAndDelete(req.params.id)
   .then(()=>res.redirect('/dashboard/artist'))
   .catch(next)
 })
 
+//Router Create Artists
+router.get("/dashboard/artist/create", (req, res, next)=>{
+  res.render("dashboard/artistCreate")
+})
+
+router.post("/dashboard/artist/create", uploader.single("picture"), (req, res, next) => {
+  const {name, isBand, description } = req.body;
+  let picture;
+  if (req.file) {
+    picture = req.file.path;
+  }
+  ArtistModel.create({
+    name,
+    isBand: isBand === "on",
+    description,
+    picture
+  })
+  .then(()=>{
+    res.redirect("/dashboard/artist")
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+});
 
 //Router Labels 
 
